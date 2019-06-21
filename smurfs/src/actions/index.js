bebc8b628cea6b2c7a8b67d951b328630ff08e5a
hsrsbd <hsraffety@gmail.com>
2019-06-21 08:07:06 -0700

    building out reducers/actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -3,6 +3,14 @@
   Be sure to export each action type so you can pull it into your reducer
 */
 
+export const FETCHING_START = 'FETCHING_START';
+export const FETCHING_SUCCESS = 'FETCHING_SUCCESS';
+export const FETCHING_FAILURE = 'FETCHING_FAILURE';
+
+export const ADDING_START = 'ADDING_START';
+export const ADDING_SUCCESS = 'ADDING_SUCCESS';
+export const ADDING_FAILURE = 'ADDING_FAILURE';
+
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -2,6 +2,15 @@
   Be sure to import in all of the action types from `../actions`
 */
 
+import {
+  FETCHING_START,
+  FETCHING_SUCCESS,
+  FETCHING_FAILURE,
+  ADDING_START,
+  ADDING_SUCCESS,
+  ADDING_FAILURE
+} from '../actions/index';
+
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
  {
@@ -14,6 +23,15 @@
  }
 */
 
+const initialState = {
+  smurfs: [],
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  updatingSmurf: false,
+  deletingSmurf: false,
+  error: ''
+};
+
 /*
   You'll only need one smurf reducer for this project.
   Feel free to export it as a default and import as rootReducer. 
@@ -21,3 +39,12 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
